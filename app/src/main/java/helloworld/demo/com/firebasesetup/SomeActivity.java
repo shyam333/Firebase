bfb6c5c0d42712b19f541f75ca2da8f0dab223ca
shyam ramesh <rlshyammdu@gmail.com>
2018-03-24 23:34:58 +0530

    first commit

---
+++ app/src/main/java/helloworld/demo/com/firebasesetup/SomeActivity.java
@@ -0,0 +1,17 @@
+package helloworld.demo.com.firebasesetup;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shyamramesh on 20/03/18.
+ */
+
+public class SomeActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_2);
+    }
+}
